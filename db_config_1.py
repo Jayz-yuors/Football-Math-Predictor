07c8af18df6ac6f3323eec6ecd0f4888838cf2f1
JAY KELUSKAR <jaykeluskar106@gmail.com>
2026-01-03 16:44:40 +0530

    Update db_config_1.py

--- db_config_1.py
+++ db_config_1.py
@@ -3,7 +3,7 @@ PG_HOST = "localhost"        # Change to your PG server
 PG_PORT = 5432
 PG_DB   = "Your DB Name"
 PG_USER = "Your DB UserName"      # <-- CHANGE THIS
-PG_PASS = "Your DB Password!"  # <-- CHANGE THIS
+PG_PASS = "Your DB Password"  # <-- CHANGE THIS
 
 def create_connection():
     conn = psycopg2.connect(
@@ -14,4 +14,5 @@ def create_connection():
         password=PG_PASS
     )
     return conn
+
 
